8af9cac02f4b15f504b266fc4633045b78118975
maylique <supercoder@AirvonSperCoder.fritz.box>
2023-12-14 10:57:11 +0100

    arrow start

---
+++ tag26/javascript_arrow1_4/main.js
@@ -0,0 +1,10 @@
+
+
+const hero = (heroName, heroPower, heroEnemy) => {
+    let a = `Mein Lieblingsheld ist ${heroName}`
+    let b = `Er hat die Fähigkeit ${heroPower}`
+    let c = `Sein größter Feind ist ${heroEnemy}`
+    console.log(a, b, c);
+}
+
+hero ('Superman', 'nicht zu sterben', 'Peter Zwegert')
