6fd28208da1f426bba10f1429766d5fae19137f9
maylique <maylique14@gmail.com>
2023-12-22 00:04:10 +0100

    added appendChild

--- tag30/js_events1_6/main.js
+++ tag30/js_events1_6/main.js
@@ -1,9 +1,22 @@
-const select = document.querySelector('#sel')
-const createEl = document.createElement('p')
-select.appendChild(createEl)
-const output = document.querySelector('#output')
+// const select = document.querySelector('#sel')
+// const createEl = document.createElement('p')
+// select.appendChild(createEl)
+// const output = document.querySelector('#output')
 
-select.addEventListener('change', ()=>{
-    console.log('yo');console.log(select.value);
-    output.textContent = `Geändert auf ${select[0].textContent}`
-})
+// select.addEventListener('change', ()=>{
+//     console.log('yo');console.log(select.value);
+//     output.textContent = `Geändert auf ${select[0].textContent}`
+// })
+
+const select = document.querySelector('#sel');
+const createEl = document.createElement('p');
+const test = document.querySelector('#test')
+test.appendChild(createEl);
+const output = createEl;
+
+select.addEventListener('change', () => {
+    console.log('yo');
+    console.log(select.value);
+    let i = select.value -1
+    output.textContent = `Geändert auf ${select[i].textContent}`;
+});
