0712cc144dd80cf0e287962b09c8925ab02088f7
maylique <maylique14@gmail.com>
2024-01-03 22:44:10 +0100

    finito

--- tag32/this/main.js
+++ tag32/this/main.js
@@ -24,3 +24,32 @@ let person = {
 };
 //gib das Profil von Anton mit diesem Code im Element mit der "person" aus.
 document.getElementById("person").innerHTML = person.profil();
+
+// 1_3
+
+let personFunction = {
+    fullName: function () {
+        return this.vorname + " " + this.nachname;
+    }
+};
+let personObjekt = {
+    vorname: "Anton",
+    nachname: "Fish"
+};
+
+let anzeigeImHTMLDokument = `<p>${personFunction.fullName.call(personObjekt)}</p>`;
+document.getElementById("person").innerHTML = anzeigeImHTMLDokument;
+
+// 1_4
+
+function fahrrad() {
+    console.log(this.name);
+}
+
+let name = "Canyon";
+let obj1 = { name: "Merida", fahrrad: fahrrad };
+let obj2 = { name: "Scott", fahrrad: fahrrad };
+
+this.fahrrad()
+obj1.fahrrad()
+obj2.fahrrad()
